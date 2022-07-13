1e35e07ec07493b48a2d1289b1ec8863e42790b7
shubham <shubham@gmail.com>
2022-07-13 16:08:46 +0530

    --- Auto Git Commit ---

--- script.groovy
+++ script.groovy
@@ -1,8 +1,5 @@
 node {
     label = "label-1"
-    stage('pull ') { 
-        git 'https://github.com/shubhamborkar25/jenkin.git'
-    }
    
     stage('test') { 
         sh 'echo \'successfully deployed\' >> home.txt' 
