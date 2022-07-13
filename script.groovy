f85f8306b475c40aad522a76f9bcbd4ffce0d32b
shubham <shubham@gmail.com>
2022-07-13 16:03:25 +0530

    --- Auto Git Commit ---

--- script.groovy
+++ script.groovy
@@ -2,9 +2,7 @@ node {
     stage('pull ') { 
         git 'https://github.com/shubhamborkar25/shubham_b.git'
     }
-    // stage('Build_vaibhav') { 
-    //     sh 'pwd >> home.txt' 
-    // }
+   
     stage('test') { 
         sh 'echo \'successfully deployed\' >> home.txt' 
     }
