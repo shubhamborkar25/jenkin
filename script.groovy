61447edf5fc633874751790a27f64e0c73a8be50
shubham <shubham@gmail.com>
2022-07-13 16:28:29 +0530

    --- Auto Git Commit ---

--- script.groovy
+++ script.groovy
@@ -2,7 +2,7 @@ node {
     label = "label-1"
    
     stage('test') { 
-        sh 'echo \'successfully deployed\' >> .txt' 
+        sh 'echo \'successfully deployed\' >> file3.txt' 
     }
     stage('Test') {
         sh 'echo "Test started" >> file3.txt' 
