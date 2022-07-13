c6cd6381ca65863f17e0a7bd1c117818ee9ffaca
shubham <shubham@gmail.com>
2022-07-13 15:39:00 +0530

    --- Auto Git Commit ---

--- script.groovy
+++ script.groovy
@@ -2,9 +2,9 @@ node {
     stage('git clone') { 
         git 'https://github.com/shubhamborkar25/shubham_b.git'
     }
-    stage('Build_vaibhav') { 
-        sh 'pwd >> home.txt' 
-    }
+    // stage('Build_vaibhav') { 
+    //     sh 'pwd >> home.txt' 
+    // }
     stage('test') { 
         sh 'echo \'successfully deployed\' >> home.txt' 
     }
